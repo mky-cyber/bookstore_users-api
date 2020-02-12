732180df86daaa274888fbf95732df469facafd5
marky <marky@benon.com>
2020-02-12 15:57:34 +1000

    Updates: - Created error handler - All errors are now handled in error handler

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,9 +1,13 @@
 package users_controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mky-cyber/bookstore_users-api/domain/users"
+	"github.com/mky-cyber/bookstore_users-api/services"
+	"github.com/mky-cyber/bookstore_users-api/utils/errors"
 )
 
 func GetUser(c *gin.Context) {
@@ -11,7 +15,21 @@ func GetUser(c *gin.Context) {
 }
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement create user")
+	var user users.User
+	fmt.Println("users", user)
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		fmt.Println("Errror:", err)
+		return
+	}
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	fmt.Println("users", result)
+	c.JSON(http.StatusCreated, result)
 }
 
 func SearchUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,18 @@
 package services
+
+import (
+	"net/http"
+
+	"github.com/mky-cyber/bookstore_users-api/domain/users"
+	"github.com/mky-cyber/bookstore_users-api/utils/errors"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return nil, nil
+
+	return &user, nil
+
+	return &user, &errors.RestErr{
+		Status: http.StatusBadRequest,
+	}
+}
